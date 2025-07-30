2759307f5af76e714e61b110eec2f4f4f410a098
Lasyaa0810 <2400030237@kluniversity.in>
2025-07-30 10:02:41 +0530

    version

--- frontendproject3/vite.config.js
+++ frontendproject3/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/frontendproject2/',
+  base: '/frontendproject3/',
   plugins: [react()],
 })
